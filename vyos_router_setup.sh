72839c56ed4037a3b7d718a3ada644a810a0c6fa
Eirik Habbestad Helleve <habbis@habbestad.org>
2022-06-09 22:31:59 +0200

    Update vyos_router_setup.sh

--- vyos_router_setup.sh
+++ vyos_router_setup.sh
@@ -4,32 +4,22 @@ source /opt/vyatta/etc/functions/script-template
 # based on this guide 
 # https://blog.kroy.io/2020/05/04/vyos-from-scratch-edition-1/?utm_source=pocket_mylist#Basic_Configuration
 
-interface_address="10.31.1.1"
-dhcp_address="10.31.1.0"
-nameserver1="10.31.1.1"
-subnet="24"
-dhcp_start="10.31.1.50"
-dhcp_end="10.31.1.200"
-
-interface="eth4"
-wan_interface="eth1"
-
 configure
 
 # set interface address
-set interfaces ethernet $interface address $interface_address/$subnet
-set interfaces ethernet $interface description LAN    
+set interfaces ethernet eth4 address 10.31.1.1/24
+set interfaces ethernet eth4 description LAN    
 
 commit
 save
 
 # dhcp server config 
-set service dhcp-server shared-network-name LAN subnet $dhcp_address/$subnet range 0 start $dhcp_start
-set service dhcp-server shared-network-name LAN subnet $dhcp_address/$subnet range 0 stop  $dhcp_end
+set service dhcp-server shared-network-name LAN subnet 10.31.1.0/24 range 0 start 10.31.1.50
+set service dhcp-server shared-network-name LAN subnet 10.31.1.0/24 range 0 stop  10.31.1.50
 
 # dns for dhcp server
-set service dhcp-server shared-network-name LAN subnet $dhcp_address/$subnet nameserver-server $interface_address
-set service dhcp-server shared-network-name LAN subnet $dhcp_address/$subnet default-router $interface_address
+set service dhcp-server shared-network-name LAN subnet 10.31.1.0/24 nameserver-server 10.31.1.1
+set service dhcp-server shared-network-name LAN subnet 10.31.1.0/24 default-router 10.31.1.1
 
 # set ssh port 
 set service ssh port 22
@@ -40,12 +30,12 @@ save
 # setup nat 
 # 16 subnet so you dont need to make a rule for each 24 subnet
 set nat source rule 100 source address '10.31.0.0/16'
-set nat source rule 100 outbound-interface $wan_interface
+set nat source rule 100 outbound-interface eth1
 set nat source rule 100 translation address masquerade
 commit
 save
 
-set service dns forwarding listen-address $nameserver1
+set service dns forwarding listen-address 10.31.1.1
 set service dns forwarding allow-from 10.31.1.0/16
 set service dns forwarding cache-size '0'
 commit
@@ -60,7 +50,7 @@ commit
 save
 
 # system dns
-set system name-server $nameserver1
+set system name-server 10.31.1.1
 commit
 save
 
@@ -109,7 +99,7 @@ set firewall name WAN-LAN rule 20 action accept
 set zone-policy zone LAN default-action drop
 set zone-policy zone LAN from WAN firewall name WAN-LAN
 set zone-policy zone LAN from LOCAL firewall name LOCAL-LAN
-set zone-policy zone LAN interface $interface
+set zone-policy zone LAN interface eth4
 
 set zone-policy zone LOCAL local-zone
 set zone-policy zone LOCAL from LAN firewall name LAN-LOCAL
@@ -119,12 +109,12 @@ set zone-policy zone LOCAL default-action drop
 
 set zone-policy zone WAN from LAN firewall name LAN-WAN
 set zone-policy zone WAN from LOCAL firewall name LOCAL-WAN
-set zone-policy zone WAN interface $wan_interface
+set zone-policy zone WAN interface eth1
 set zone-policy zone WAN default-action drop
 
 # wan interface dhcp
-set interfaces ethernet $wan_interface address dhcp
-set interfaces ethernet $wan_interface description WAN
+set interfaces ethernet eth1 address dhcp
+set interfaces ethernet eth1 description WAN
 
 commit
 save
